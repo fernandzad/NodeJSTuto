2a576fcee479e12ab392fcba0686fea0ea6a2e32
ad <adrian.fernandez@idom.com>
2019-11-27 23:21:15 -0600

    String template, JSON to string

--- require.js
+++ require.js
@@ -8,7 +8,15 @@ let user = os.hostname();
 // console.log(so);
 // console.log(user);
 
+//parse JSON to string
 fs.appendFile('prueba.txt','Información del CPU: ' + JSON.stringify(cpu), function(error){
+    if(error){
+        console.log('Existió un error!'); 
+    }
+});
+
+//string template
+fs.appendFile('prueba2.txt',`User: ${user}, Operative System: ${so}`, function(error){
     if(error){
         console.log('Existió un error!'); 
     }
