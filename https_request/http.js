87986146382a74ceabc9d9d7a25235069c6e0508
Jesusfer2575 <adrian.fernandez@idom.com>
2019-12-02 21:07:55 -0600

    https a GitHub

--- https_request/http.js
+++ https_request/http.js
@@ -2,6 +2,8 @@
 const https = require('https');
 
 let username = 'Jesusfer2575';
+let main_path = '/users/' + username;
+let followers_path = '/users/' + username + '/followers';
 
 let CHROME_USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36';
 let FIREFOX_USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1';
@@ -9,7 +11,25 @@ let IE_USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.
 
 let options = {
     host: 'api.github.com',
-    path: '/users/' + username,
+    path: followers_path,
     method: 'GET',
     headers: { 'user-agent': IE_USER_AGENT}
 };
+
+let request = https.request(options, (response) => {
+    let body = '';
+    response.on('data', (out) => {
+        body += out;
+    });
+
+    response.on('end', (out) => {
+        let json = JSON.parse(body);
+        console.log(json);
+    });
+});
+
+request.on('error', (e)=>{
+    console.log(e);
+});
+
+request.end();
